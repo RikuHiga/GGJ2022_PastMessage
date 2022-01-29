293dc94612e1b7d90815a116970707d770e1328d
e195760 <e195670@ie.u-ryukyu.ac.jp>
2022-01-30 01:31:44 +0900

    カメラワークをなめらかにした

--- PastMessage/Assets/CamControl.cs
+++ PastMessage/Assets/CamControl.cs
@@ -13,21 +13,24 @@ public class CamControl : MonoBehaviour
     private float CAM_DIST = 20;//カメラがいる距離。平行透視なので近づいても拡大されない。
     private float CAM_HEIGHT_ANGLE = 30;//カメラが上方何度の位置から映すか。
     private float CAM_HORIZON_ANGLE = 45;
-    private float camSize = 3.11f;
+    private float camSize = 4f;
     private Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-
+        camera=this.gameObject.GetComponent<Camera>();
+        camera.orthographicSize = camSize;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.rotation = Quaternion.Euler(CAM_HEIGHT_ANGLE, 90-CAM_HORIZON_ANGLE, 0);
-        //-x,-z方向に45°、y方向に上方30°
         float bottom = CAM_DIST * Mathf.Cos(CAM_HEIGHT_ANGLE * Mathf.Deg2Rad);
         Vector3 cameraDiff = new Vector3(-(bottom*Mathf.Cos(CAM_HORIZON_ANGLE*Mathf.Deg2Rad)), CAM_DIST * Mathf.Sin(CAM_HEIGHT_ANGLE * Mathf.Deg2Rad), -(bottom * Mathf.Sin(CAM_HORIZON_ANGLE * Mathf.Deg2Rad)));
-        transform.position = tracedObj.transform.position + cameraDiff;
+        //transform.position = tracedObj.transform.position + cameraDiff;
+        transform.position = Vector3.Lerp(transform.position, tracedObj.transform.position + cameraDiff, Time.deltaTime);
+
+        //Vector3 viewportPos = camera.WorldToViewportPoint(tracedObj.transform.position);
     }
 }
