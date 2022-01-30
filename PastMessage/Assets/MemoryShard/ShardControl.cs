65a1e666fdcfd0b50d4e43a03a4a19fe26f18fd7
e195760 <e195670@ie.u-ryukyu.ac.jp>
2022-01-30 13:52:29 +0900

    記憶のカケラを取ったときの演出を追加

--- PastMessage/Assets/MemoryShard/ShardControl.cs
+++ PastMessage/Assets/MemoryShard/ShardControl.cs
@@ -9,6 +9,7 @@ public class ShardControl : MonoBehaviour
     float timer = 0;
     bool trigger = false;
     public ParticleSystem particle;
+    public GameObject hanabi;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,17 @@ public class ShardControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = new Vector3(transform.position.x, 1f + Mathf.Sin(Time.time)*0.3f, transform.position.z);
         transform.Rotate(0, 0, 50*Time.deltaTime);
         if (trigger)
         {
             timer += Time.deltaTime*1.5f;
             transform.localScale = new Vector3(defaultSize.x * curve.Evaluate(timer), defaultSize.y * curve.Evaluate(timer), defaultSize.z * curve.Evaluate(timer));
+            if (timer >= 1.0f)
+            {
+                Instantiate(hanabi, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
     }
 
